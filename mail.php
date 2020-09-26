c9185203bcfcc29194bb0437ea8327dfd433da52
trif <trif.tz@gmail.com>
2020-09-26 14:02:40 +0200

    Min

--- mail.php
+++ mail.php
@@ -1,14 +1,15 @@
 <?php
 $name = $_POST['name'];
-$businessName = $_POST['businessName'];
+// $businessName = $_POST['businessName'];
 $email = $_POST['email'];
-$website = $_POST['website'];
-$menuExists = $_POST['menuExists'];
-$phone = $_POST['phone'];
-$call = $_POST['call'];
+// $website = $_POST['website'];
+// $menuExists = $_POST['menuExists'];
+// $phone = $_POST['phone'];
+// $call = $_POST['call'];
 $type = $_POST['request'];
 $message = $_POST['message'];
-$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Menu exists: $menuExists \n Website: $website \n Request: $type \n Message: $message";
+// $formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Menu exists: $menuExists \n Website: $website \n Request: $type \n Message: $message";
+$formcontent=" From: $name \n Request: $type \n Message: $message";
 $recipient = "trif.tz@gmail.com";
 $subject = "Anepafo Contact Form";
 $mailheader = "From: $email \r\n";
